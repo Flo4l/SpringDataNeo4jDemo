706b316ef40dd5b1eec72b90d32413405307460c
Florian Albers <Florian.Albers@adesso.de>
2019-10-08 09:54:25 +0200

    Added docker compatibility

--- src/test/java/com/syn/SpringDataNeo4jDemo/ApplicationTests.java
+++ src/test/java/com/syn/SpringDataNeo4jDemo/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringdDataNeo4jDemoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
